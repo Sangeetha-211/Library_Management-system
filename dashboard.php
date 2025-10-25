7dd2bf02316df3be8de15707dc9bbf59228ce7d2
Sangeetha-211 <sangeethapandisingh1212@gmail.com>
2025-10-25 15:26:11 +0530

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -2,13 +2,14 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if (strlen($_SESSION['login']) == 0) {
-  header('location:index.php');
-} else { ?>
-  <!DOCTYPE html>
-  <html xmlns="http://www.w3.org/1999/xhtml">
-
-  <head>
+if(strlen($_SESSION['login'])==0)
+  { 
+header('location:index.php');
+}
+else{?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
@@ -22,140 +23,95 @@ if (strlen($_SESSION['login']) == 0) {
     <link href="assets/css/style.css" rel="stylesheet" />
     <!-- GOOGLE FONT -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-    <style>
-      body {
-        background-image: url('assets/img/library-books.jpg');
-        /* Replace with the path to your image */
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        position: relative;
-      }
 
-      /* Add a semi-transparent overlay */
-      body::before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.5);
-        /* Black overlay with 50% opacity */
-        z-index: -1;
-      }
-
-      .content-wrapper {
-        color: #fff;
-        /* Make text white for better contrast */
-      }
-
-      .alert {
-        background-color: rgba(255, 255, 255, 0.8);
-        /* Semi-transparent background for widgets */
-        color: #000;
-        /* Black text for widgets */
-      }
-
-      h4.header-line {
-        color: #fff;
-        /* White header text */
-      }
-    </style>
-  </head>
-
-  <body>
-    <!------MENU SECTION START-->
-    <?php include('includes/header.php'); ?>
-    <!-- MENU SECTION END-->
+</head>
+<body>
+      <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
     <div class="content-wrapper">
-      <div class="container">
+         <div class="container">
         <div class="row pad-botm">
-          <div class="col-md-12">
-            <h4 class="header-line">User DASHBOARD</h4>
-
-          </div>
-
-        </div>
-
-        <div class="row">
-
-
-          <a href="listed-books.php">
-            <div class="col-md-4 col-sm-4 col-xs-6">
-              <div class="alert alert-success back-widget-set text-center">
-                <i class="fa fa-book fa-5x"></i>
-                <?php
-                $sql = "SELECT id from tblbooks ";
-                $query = $dbh->prepare($sql);
-                $query->execute();
-                $results = $query->fetchAll(PDO::FETCH_OBJ);
-                $listdbooks = $query->rowCount();
-                ?>
-                <h3><?php echo htmlentities($listdbooks); ?></h3>
-                Books Listed
-              </div>
-            </div>
-          </a>
-
-          <div class="col-md-4 col-sm-4 col-xs-6">
-            <div class="alert alert-warning back-widget-set text-center">
-              <i class="fa fa-recycle fa-5x"></i>
-              <?php
-              $rsts = 0;
-              $sid = $_SESSION['stdid'];
-              $sql2 = "SELECT id from tblissuedbookdetails where StudentID=:sid and (RetrunStatus=:rsts || RetrunStatus is null || RetrunStatus='')";
-              $query2 = $dbh->prepare($sql2);
-              $query2->bindParam(':sid', $sid, PDO::PARAM_STR);
-              $query2->bindParam(':rsts', $rsts, PDO::PARAM_STR);
-              $query2->execute();
-              $results2 = $query2->fetchAll(PDO::FETCH_OBJ);
-              $returnedbooks = $query2->rowCount();
-              ?>
-
-              <h3><?php echo htmlentities($returnedbooks); ?></h3>
-              Books Not Returned Yet
-            </div>
-          </div>
-
-          <?php
-
-          $ret = $dbh->prepare("SELECT id from tblissuedbookdetails where StudentID=:sid");
-          $ret->bindParam(':sid', $sid, PDO::PARAM_STR);
-          $ret->execute();
-          $results22 = $ret->fetchAll(PDO::FETCH_OBJ);
-          $totalissuedbook = $ret->rowCount();
-          ?>
-
-
-          <a href="issued-books.php">
-            <div class="col-md-4 col-sm-4 col-xs-6">
-              <div class="alert alert-success back-widget-set text-center">
-                <i class="fa fa-book fa-5x"></i>
-                <h3><?php echo htmlentities($totalissuedbook); ?></h3>
-                Total Issued Books
-              </div>
-            </div>
-          </a>
-
-
-
-
+            <div class="col-md-12">
+                <h4 class="header-line">User DASHBOARD</h4>
+                
+                            </div>
 
         </div>
-      </div>
+             
+             <div class="row">
+
+
+<a href="listed-books.php">
+<div class="col-md-4 col-sm-4 col-xs-6">
+ <div class="alert alert-success back-widget-set text-center">
+ <i class="fa fa-book fa-5x"></i>
+<?php 
+$sql ="SELECT id from tblbooks ";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$listdbooks=$query->rowCount();
+?>
+<h3><?php echo htmlentities($listdbooks);?></h3>
+Books Listed
+</div></div></a>
+             
+               <div class="col-md-4 col-sm-4 col-xs-6">
+                      <div class="alert alert-warning back-widget-set text-center">
+                            <i class="fa fa-recycle fa-5x"></i>
+<?php 
+$rsts=0;
+ $sid=$_SESSION['stdid'];
+$sql2 ="SELECT id from tblissuedbookdetails where StudentID=:sid and (RetrunStatus=:rsts || RetrunStatus is null || RetrunStatus='')";
+$query2 = $dbh -> prepare($sql2);
+$query2->bindParam(':sid',$sid,PDO::PARAM_STR);
+$query2->bindParam(':rsts',$rsts,PDO::PARAM_STR);
+$query2->execute();
+$results2=$query2->fetchAll(PDO::FETCH_OBJ);
+$returnedbooks=$query2->rowCount();
+?>
+
+                            <h3><?php echo htmlentities($returnedbooks);?></h3>
+                          Books Not Returned Yet
+                        </div>
+                    </div>
+
+<?php 
+
+$ret =$dbh -> prepare("SELECT id from tblissuedbookdetails where StudentID=:sid");
+$ret->bindParam(':sid',$sid,PDO::PARAM_STR);
+$ret->execute();
+$results22=$ret->fetchAll(PDO::FETCH_OBJ);
+$totalissuedbook=$ret->rowCount();
+?>
+
+
+<a href="issued-books.php">
+<div class="col-md-4 col-sm-4 col-xs-6">
+ <div class="alert alert-success back-widget-set text-center">
+ <i class="fa fa-book fa-5x"></i>
+      <h3><?php echo htmlentities($totalissuedbook);?></h3>
+Total Issued Books
+</div></div></a>
+
+
+
+
+
+        </div>    
     </div>
-    <!-- CONTENT-WRAPPER SECTION END-->
-    <?php include('includes/footer.php'); ?>
-    <!-- FOOTER SECTION END-->
+    </div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+<?php include('includes/footer.php');?>
+      <!-- FOOTER SECTION END-->
     <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
     <!-- CORE JQUERY  -->
     <script src="assets/js/jquery-1.10.2.js"></script>
     <!-- BOOTSTRAP SCRIPTS  -->
     <script src="assets/js/bootstrap.js"></script>
-    <!-- CUSTOM SCRIPTS  -->
+      <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
-  </body>
-
-  </html>
+</body>
+</html>
 <?php } ?>
